efbf7003ab0450e2082e01923bc35fe7d1f0883a
Morgan Brown <79729461+MorganBrownDev@users.noreply.github.com>
2022-07-03 16:07:46 +1000

    Update learn.js

--- js/learn.js
+++ js/learn.js
@@ -4,3 +4,14 @@ btn.addEventListener('click', (e) => {
     e.preventDefault();
     console.log(e.target);
 });
+
+const vibes = [
+    "... and you are awesome!",
+    "... have a wonderful day!",
+    "... and you've got this!",
+    "... and so is this puppy!"
+];
+
+var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+document.querySelector(".vibe").append(vibe);
